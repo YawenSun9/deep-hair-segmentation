6f4268c9ab337c7c8f49e6cfd578a0f566ec3a0c
yulian <name@email.com>
2019-05-29 19:37:12 -0700

    gradient loss

--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class SegmentationLosses(object):
     def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
@@ -15,8 +16,52 @@ class SegmentationLosses(object):
             return self.CrossEntropyLoss
         elif mode == 'focal':
             return self.FocalLoss
+        elif mode == 'hair':
+            return self.HairMatLoss
         else:
             raise NotImplementedError
+
+    def HairMatLoss(self, logit, target, weight=0.5):
+        loss1 = self.CrossEntropyLoss(logit, target)
+        loss2 = weight * self.GradientConsistencyLoss(logit, target)
+        print (loss1, loss2)
+        return  loss1
+
+
+    def GradientConsistencyLoss(self, logit, target):
+        sobel_kernel_x = torch.Tensor(
+                    [[1.0, 0.0, -1.0],
+                    [2.0, 0.0, -2.0],
+                    [1.0, 0.0, -1.0]])
+        if self.cuda:
+            sobel_kernel_x = sobel_kernel_x.cuda()
+        sobel_kernel_x = sobel_kernel_x.view((1,1,3,3))
+        N, H, W = target.shape
+
+        I_x = F.conv2d(logit[:,1:2, ...], sobel_kernel_x, padding = 1)
+        M_x = F.conv2d(target.view(N,1,H,W), sobel_kernel_x, padding = 1)
+
+        sobel_kernel_y = torch.Tensor(
+                    [[1.0, 2.0, 1.0],
+                    [0.0, 0.0, 0.0],
+                    [-1.0, -2.0, -1.0]])
+        if self.cuda:
+            sobel_kernel_y = sobel_kernel_y.cuda()
+        sobel_kernel_y = sobel_kernel_y.view((1,1,3,3))
+
+        I_y = F.conv2d(logit[:,1:2, ...], sobel_kernel_y, padding = 1)
+        M_y = F.conv2d(target.view(N,1,H,W), sobel_kernel_y, padding = 1)
+
+        Imag_pow = torch.pow(I_x,2) + torch.pow(I_y,2) + 1e-30
+        Mmag_pow = torch.pow(M_x,2) + torch.pow(M_y,2) + 1e-30
+        Mmag = torch.sqrt(Mmag_pow)
+
+        rang_grad = 1 - torch.pow(I_x*M_x + I_y*M_y,2) / Imag_pow / Mmag_pow
+        loss = torch.sum(torch.mul(Mmag, rang_grad))/torch.sum(Mmag)
+
+        if self.batch_average:
+            loss /= N
+        return loss
 
     def CrossEntropyLoss(self, logit, target):
         n, c, h, w = logit.size()
