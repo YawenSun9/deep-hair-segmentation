c883a2a5415e949a86004547113ab2d6d8a73bda
unknown <wangdachui2014@hotmail.com>
2019-05-29 19:53:45 -0700

    sobel and matting

--- utils/sobelOperator.py
+++ utils/sobelOperator.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt 
+from PIL import Image
 import cv2
 import torch
 import torch.nn as nn
@@ -18,7 +19,7 @@ class Sobel:
     At each point the result is either the corresponding gradient vector or the norm2 of this vector
     Convoluting the img on x and y derection 
 
-    argvs: img(tensor) 2d
+    argvs: img(tensor) 2d ----not sure----
     '''
     def __init__(self, img):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -30,7 +31,9 @@ class Sobel:
                     [[1.0, 2.0, 1.0],
                     [0.0, 0.0, 0.0],
                     [-1.0, -2.0, -1.0]]).to(self.device).view((1,1,3,3))
-        self.img = img
+        self.H = img.shape[0]
+        self.W = img.shape[1]
+        self.img = img.view((1,1,self.H,self.W))
 
     def x_only_mask(self):
         G_x = F.conv2d(self.img, self.sobel_kernel_x, stride=1, padding=1)
@@ -41,33 +44,29 @@ class Sobel:
     def x_y_mask(self):
         G_x = F.conv2d(self.img, self.sobel_kernel_x, stride=1, padding=1)
         G_y = F.conv2d(self.img, self.sobel_kernel_y, stride=1, padding=1)
-        G_xy = torch.sqrt(torch.pow(G_x,2)+ torch.pow(G_y,2))
+        G_xy = torch.sqrt(torch.pow(G_x,2)+ torch.pow(G_y,2)).clamp(max=255)
         return G_xy
 
 '''
 # demo
 
 if __name__ == "__main__":
-    # read file
-    img = cv2.cvtColor(cv2.imread("sol_key.png"), cv2.COLOR_BGR2GRAY) # GRAYs
+    img = np.asarray(Image.open('sol_key.png').convert('L')).astype('float')
     H, W = img.shape
-    img = torch.from_numpy(img).float().view((1,1,H,W))
-    
-    # print(img.shape)
-    # apply sobel
-    img1 = Sobel(img).x_only_mask()
-    img2 = Sobel(img).y_only_mask()
-    img3 = Sobel(img).x_y_mask()
-    
-    # img1 = cv2.cvtColor(img1, cv2.COLOR_GRAY2RGB)
-    # img2 = cv2.cvtColor(img2, cv2.COLOR_GRAY2RGB)
-    # img3 = cv2.cvtColor(img3, cv2.COLOR_GRAY2RGB)
-    
-    # visualize
-    plt.subplot(3,1,1); plt.imshow(img1.view((H,W)));plt.axis('off');plt.title('sobel_x_only')
-    plt.subplot(3,1,2); plt.imshow(img2.view((H,W)));plt.axis('off');plt.title('sobel_y_only')
-    plt.subplot(3,1,3); plt.imshow(img3.view((H,W)));plt.axis('off');plt.title('sobel_x_y')
+    img = torch.from_numpy(img).float()
+    img = Sobel(img).x_y_mask()
+    img = Image.fromarray(img.numpy().reshape((H,W)).astype('uint8'))
+    plt.imshow(img)
     plt.show()
-
-    # output file
+    
+    
+    # # it is total the same as using Image, but the output is wierd
+    # img = cv2.cvtColor(cv2.imread("p.jpg"), cv2.COLOR_BGR2GRAY) # read file and to GRAYs image 
+    # H, W = img.shape
+    # img = torch.from_numpy(img).float().view((1,1,H,W)) # to tensor
+    # img = Sobel(img).x_y_mask() # apply sobel
+    # img = cv2.cvtColor(img.numpy().reshape((H,W)), cv2.COLOR_GRAY2RGB) # to image
+    # plt.imshow(img)
+    # plt.show()
 '''
+
