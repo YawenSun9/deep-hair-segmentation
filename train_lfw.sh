d4661e208575a4c2582165fd0d3dce4e7ff9c749
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-06-02 22:21:52 +0000

    fix lr

--- train_lfw.sh
+++ train_lfw.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python train.py --backbone resnet --lr 0.001 --workers 1 --epochs 100 --batch-size 32 --gpu-ids 0 --checkname deeplab-resnet-backbone --out-stride 16 --eval-interval 1 --dataset lfw
+CUDA_VISIBLE_DEVICES=0 python train.py --backbone resnet --lr 0.001 --workers 1 --epochs 100 --batch-size 32 --gpu-ids 0 --checkname deeplab-resnet-base --weight-decay 0 --eval-interval 1 --dataset lfw
